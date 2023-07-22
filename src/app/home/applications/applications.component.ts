f736ffda716e3b012c0fccb243f18e7afde03e5d
Andy Wu <dancedargon@gmail.com>
2023-07-23 01:10:41 +0800

    finsih full screen

--- src/app/home/applications/applications.component.ts
+++ src/app/home/applications/applications.component.ts
@@ -1,13 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
 @Component({
   selector: 'app-applications',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,VirtualScrollerModule,ScrollPanelModule,DropdownModule,DividerModule,ButtonModule,InputTextModule,SplitButtonModule,ToolbarModule],
   templateUrl: './applications.component.html',
   styleUrls: ['./applications.component.scss']
 })
 export class ApplicationsComponent {
   @Input() title!: string;
+
 }
--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -4,11 +4,11 @@ import { ApplicationsComponent } from './applications/applications.component';
 import { BreakingNewsComponent } from './breaking-news/breaking-news.component';
 export const Homeroutes: Routes = [
   {
-    path: '',
-    component: BreakingNewsComponent,
-  },
-  {
     path : 'applications',
     component: ApplicationsComponent,
   },
+  {
+    path : 'breaking-news',
+    component: BreakingNewsComponent,
+  }
 ];
