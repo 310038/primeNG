4af940c5a23f4e4822a1e819bf215ef4f3a72b19
Andy Wu <dancedargon@gmail.com>
2023-07-24 01:20:25 +0800

    commit

--- src/app/home/breaking-news/breaking-news.component.ts
+++ src/app/home/breaking-news/breaking-news.component.ts
@@ -9,10 +9,11 @@ import { InputTextModule } from 'primeng/inputtext';
 import { CheckboxModule } from 'primeng/checkbox';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { PanelModule } from 'primeng/panel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 @Component({
   selector: 'app-breaking-news',
   standalone: true,
-  imports: [CommonModule,PanelModule,RadioButtonModule,CheckboxModule,InputTextModule,ButtonModule,ToolbarModule,SplitterModule,CardModule,DividerModule],
+  imports: [CommonModule,ScrollPanelModule,PanelModule,RadioButtonModule,CheckboxModule,InputTextModule,ButtonModule,ToolbarModule,SplitterModule,CardModule,DividerModule],
   templateUrl: './breaking-news.component.html',
   styleUrls: ['./breaking-news.component.scss']
 })
